e93fc683c18014a319c0b09d210e5a2f641bde79
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-06-12 20:33:34 +0200

    image processing be

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,60 +1,65 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const replaceColor = require('replace-color')
-const {writeFileSync} = require("fs");
+const replaceColor = require("replace-color");
+const { writeFileSync } = require("fs");
 const Jimp = require("jimp");
 
-router.post('/upload', async function (req, res, next) {
-    try {
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+router.post("/upload", async (req, res, next) => {
+  try {
+    const color = req.body.color;
+    const deltaE = req.body.deltaE;
 
-        // to declare some path to store your converted image, to the root directory of the project
-        const path = 'public/images/' + Date.now() + '.png';
+    // to declare some path to store your converted image, to the root directory of the project
+    const imageName = Date.now() + ".png";
+    const path = "public/images/" + imageName;
 
-        const imgdata = req.body.image.contents;
+    const imgdata = req.body.image.contents;
 
-        // to convert base64 format into random filename
-        const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+    // to convert base64 format into random filename
+    const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, "");
 
-        await writeFileSync(path, base64Data, {encoding: 'base64'});
+    await writeFileSync(path, base64Data, { encoding: "base64" });
 
-        const jimpOverlayImage = await Jimp.read('https://www.goodfreephotos.com/albums/other-landscapes/mountain-peaks-clouds-and-landscape.jpg');
+    await replaceColor({
+      image: path,
+      colors: {
+        type: "hex",
+        targetColor: color, //3bb14b
+        replaceColor: "#00FFFFFF",
+      },
+      deltaE: deltaE,
+    })
+      .then((jimpObject) => {
+        jimpObject.write(path);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-        await replaceColor({
-            image: path,
-            colors: {
-                type: 'hex',
-                targetColor: '#3bb14b',
-                replaceColor: '#00FFFFFF'
-            },
-            deltaE: 35
-        }).then((jimpObject) => {
-            jimpObject.write(path);
+    await Jimp.read(
+      "https://www.goodfreephotos.com/albums/other-landscapes/mountain-peaks-clouds-and-landscape.jpg",
+      (err, overlay) => {
+        Jimp.read(path, (err, image) => {
+          image
+            .composite(overlay, 0, 0, {
+              mode: Jimp.BLEND_DESTINATION_OVER,
+            })
+            .write(path); // save
 
-        }).catch((err) => {
-            console.log(err);
-
+          return res.send({
+            url:
+              req.protocol + "://" + req.get("host") + "/images/" + imageName,
+          });
         });
-
-
-           Jimp.read(path, (err, image) => {
-
-                if (err) throw err;
-                image   // resize
-                    .composite(jimpOverlayImage, 0, 0, {
-                        mode: Jimp.BLEND_DESTINATION_OVER,
-                    })
-                    .write(path); // save
-            }
-        );
-
-        return res.send(path);
-
-    } catch (e) {
-        next(e);
-    }
-
-
+      }
+    );
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
