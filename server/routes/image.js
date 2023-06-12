1ed93f0a25cedd8546cf42aba7ee0df59964a61a
Robbe Bierebeeck <robbebierebeeck@Robbes-MacBook-Pro-6.local>
2023-06-12 22:41:45 +0200

    refractor: rm gimp processing

--- server/routes/image.js
+++ server/routes/image.js
@@ -1,65 +1,79 @@
 const express = require("express");
 const router = express.Router();
 const replaceColor = require("replace-color");
-const { writeFileSync } = require("fs");
+const {writeFileSync} = require("fs");
 const Jimp = require("jimp");
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 router.post("/upload", async (req, res, next) => {
-  try {
-    const color = req.body.color;
-    const deltaE = req.body.deltaE;
+    try {
+        const color = req.body.color;
+        const deltaE = req.body.deltaE;
 
-    // to declare some path to store your converted image, to the root directory of the project
-    const imageName = Date.now() + ".png";
-    const path = "public/images/" + imageName;
+        // to declare some path to store your converted image, to the root directory of the project
+        const imageName = Date.now() + ".png";
+        const path = "public/images/" + imageName;
 
-    const imgdata = req.body.image.contents;
+        const imgdata = req.body.image.contents;
 
-    // to convert base64 format into random filename
-    const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, "");
+        // to convert base64 format into random filename
+        const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, "");
 
-    await writeFileSync(path, base64Data, { encoding: "base64" });
 
-    await replaceColor({
-      image: path,
-      colors: {
-        type: "hex",
-        targetColor: color, //3bb14b
-        replaceColor: "#00FFFFFF",
-      },
-      deltaE: deltaE,
-    })
-      .then((jimpObject) => {
-        jimpObject.write(path);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        writeFileSync(__dirname + "/../public/images/" + imageName, base64Data, "base64");
 
-    await Jimp.read(
-      "https://www.goodfreephotos.com/albums/other-landscapes/mountain-peaks-clouds-and-landscape.jpg",
-      (err, overlay) => {
-        Jimp.read(path, (err, image) => {
-          image
-            .composite(overlay, 0, 0, {
-              mode: Jimp.BLEND_DESTINATION_OVER,
+
+        await replaceColor({
+            image: __dirname + "/../public/images/" + imageName,
+            colors: {
+                type: "hex",
+                targetColor: color, //3bb14b
+                replaceColor: "#00FFFFFF",
+            },
+            deltaE: deltaE,
+        })
+            .then((jimpObject) => {
+                jimpObject.write(__dirname + "/../public/images/" + imageName);
             })
-            .write(path); // save
+            .catch((err) => {
+                console.log(err);
+            });
 
-          return res.send({
+
+        return res.send({
             url:
-              req.protocol + "://" + req.get("host") + "/images/" + imageName,
-          });
+                req.protocol + "://" + req.get("host") + "/images/" + imageName,
         });
-      }
-    );
-  } catch (e) {
-    next(e);
-  }
+
+
+
+
+        /*  await Jimp.read(
+              "https://www.goodfreephotos.com/albums/other-landscapes/mountain-peaks-clouds-and-landscape.jpg",
+              (err, overlay) => {
+                  Jimp.read( __dirname + "/../public/images/" + imageName
+                      , (err, image) => {
+                      image
+                          .composite(overlay, 0, 0, {
+                              mode: Jimp.BLEND_DESTINATION_OVER,
+                          })
+                          .write(
+                              __dirname + "/../public/images/" + imageName
+                          ); // save
+
+                      return res.send({
+                          url:
+                              req.protocol + "://" + req.get("host") + "/images/" + imageName,
+                      });
+                  });
+              }
+          );*/
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = router;
